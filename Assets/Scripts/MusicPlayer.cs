33134d1503e16aa19772e220f09fb5ea1adc3d67
danijel <danijel.husakovic@gmail.com>
2018-02-23 20:30:11 +0100

    Added player's ship. It moves. It is constricted. Fix later: shipWidth/2 instead of current stupid padding. Imported THEIR menus. Moved MusicPlayer from start to awake.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	void Start () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float padding = 1f;
+
+	private float speed;
+	private float xMin, xMax;
+	
+	void Start(){
+		speed = 15f;
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f,0f, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f,0f, distance));
+		xMin = leftmost.x + padding;
+		xMax = rightmost.x - padding;
+	}
+	
+	
+	void Update () {
+	Move();	
+	}
+	
+	public void Move(){
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += Vector3.left * speed * Time.deltaTime;		
+		} else if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+			
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+}
