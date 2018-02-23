043c53449b95644da79fdb4fdfbaabe98f5195cd
danijel <danijel.husakovic@gmail.com>
2018-02-23 18:51:47 +0100

    First commit. Imported the menu system that I exported by myself from Number Wizard UI

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("LoadLevel method entered for " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Quit requested!");
+		Application.Quit();
+	}
+}
